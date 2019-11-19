7bfa45069d8f9a1b401c1ea86f6d30c75ecc7f37
Mathew Gitata <gitataIT@gmail.com>
2019-11-19 21:35:54 +0300

    Create root route to render homepage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,20 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.attribute("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
