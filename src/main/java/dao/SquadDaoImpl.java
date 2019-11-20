4606488e0358cda3d859f5217c1d9cf8c0f2cf08
Mathew Gitata <gitataIT@gmail.com>
2019-11-20 05:31:36 +0300

    Add tests to ensure squad objects added succesfully and all instances gotten succesfully

--- src/main/java/dao/SquadDaoImpl.java
+++ src/main/java/dao/SquadDaoImpl.java
@@ -2,18 +2,26 @@ package dao;
 
 import models.Squad;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SquadDaoImpl implements SquadDao {
+    private List<Squad> squads;
+
+    public SquadDaoImpl() {
+        squads = new ArrayList<>();
+    }
+
     @Override
     public boolean addSquad(Squad squad) {
-        return false;
+        return squads.add(squad);
     }
 
     @Override
     public List<Squad> findAllSquads() {
-        return null;
+        return new ArrayList<>(squads);
     }
+
 
     @Override
     public Squad findSquadBySlug(String slug) {
